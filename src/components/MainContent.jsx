c8545a87fabf69a093ca03a16acad955224333d3
kingithegreat <139932842+kingithegreat@users.noreply.github.com>
2024-08-26 12:34:12 +1200

    changed path

--- src/components/MainContent.jsx
+++ src/components/MainContent.jsx
@@ -27,7 +27,7 @@ function MainContent(props) {
       </p> 
       {/* Display the SCP image */}
       <div className="text-center mb-4">
-        <img className="images rounded" src={"/src/assets/" + scp.Image} alt="SCP image" />
+        <img className="images rounded" src={"/assets/" + scp.Image} alt="SCP image" />
       </div>
     </div>
   );
